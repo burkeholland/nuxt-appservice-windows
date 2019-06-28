64c9e201cf1cd1f542520ef4af9cd2f27ca8dbe2
Burke Holland <burkeholland@gmail.com>
2019-06-28 12:11:43 -0500

    update nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -62,5 +62,10 @@ module.exports = {
      ** You can extend webpack config here
      */
     extend(config, ctx) {}
+  },
+  server: {
+    port: 8080, // default: 3000
+    host: '0.0.0.0', // default: localhost,
+    timing: false
   }
 }
